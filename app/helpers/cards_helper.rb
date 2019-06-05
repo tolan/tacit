8de46a305992fdbf74d8080a96bfae2de77bb9e3
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-06-05 17:21:54 +0200

    add images in assets

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,10 @@
 module CardsHelpers
+  def trend?(the_subscription)
+    # return true if there is a trend
+  end
+
+  def up_or_down(the_subscription)
+    the_subscription
+    # return true if the last transaction was
+  end
 end
