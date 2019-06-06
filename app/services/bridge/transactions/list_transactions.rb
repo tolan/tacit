8211df8e070f6314b042ed47032fa84ed5ff7afa
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-06 12:00:33 +0200

    refacto 1

---
+++ app/services/bridge/transactions/list_transactions.rb
@@ -0,0 +1,17 @@
+class Bridge::Transactions::ListTransactions
+  require 'rest-client'
+
+  def self.call(access_token)
+    url = "https://sync.bankin.com/v2/transactions"
+    params = {
+      client_id: ENV["bridge_client_id"],
+      client_secret: ENV["bridge_client_secret"]
+    }
+    response = RestClient.get(url, {
+      params: params,
+      "Bankin-Version": "2018-06-15",
+      "Authorization": "Bearer #{access_token}"
+    })
+    JSON.parse(response.body)["resources"]
+  end
+end
