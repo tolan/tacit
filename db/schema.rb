504b129ab7dc1449874d3f65b8f56c534ea59fa3
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-06-06 11:38:14 +0200

    amount now in float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_180236) do
+ActiveRecord::Schema.define(version: 2019_06_06_092837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_180236) do
 
   create_table "operations", force: :cascade do |t|
     t.string "description"
-    t.integer "amount"
+    t.float "amount"
     t.date "date"
     t.bigint "bridge_id"
     t.bigint "user_id"
