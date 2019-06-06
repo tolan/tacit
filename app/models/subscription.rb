48005df13838aff209eeb39aa9ddff93a32ebd7a
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-06-06 12:39:19 +0200

    create methods in subscription method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,24 @@
 class Subscription < ApplicationRecord
   belongs_to :operator
   has_many :operations
+
+  def new_sub?
+
+  end
+
+  def trend
+
+  end
+
+  def avg_fee
+
+  end
+
+  def payment_frequency
+
+  end
+
+  def time_before_next_payment
+
+  end
 end
