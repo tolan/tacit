342bfce3f88d2846fbfa482420ed797569c75307
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-06-06 12:51:55 +0200

    create new sub, avg fee and trend methods in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,15 +3,25 @@ class Subscription < ApplicationRecord
   has_many :operations
 
   def new_sub?
-
-  end
-
-  def trend
-
+    operations.count == 1
   end
 
   def avg_fee
+    operations.average(:amount)
+  end
 
+
+  # give infos relative to subscription fees variation
+  def trend
+    if new_sub?
+      "none"
+    elsif avg_fee == operations.last.amount
+      "flat fee"
+    elsif operations.last.amount > operations[-2].amount
+      "up"
+    else
+      "down"
+    end
   end
 
   def payment_frequency
