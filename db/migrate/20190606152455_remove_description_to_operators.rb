8d3102731b19bbccb8f50d0c4839bd809396b78c
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-06-06 18:11:15 +0200

    seed done

---
+++ db/migrate/20190606152455_remove_description_to_operators.rb
@@ -0,0 +1,4 @@
+class RemoveDescriptionToOperators < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_092837) do
+ActiveRecord::Schema.define(version: 2019_06_06_152455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_092837) do
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "regex"
   end
 
   create_table "subscriptions", force: :cascade do |t|
