30b2c175335ec5cbf9a3a3769d4d66ea7c83212d
Ssyleric <esylvius@gmail.com>
2019-06-07 15:46:08 +0200

    Controller-Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+    resources :subscriptions, only: [ :index, :show ]
+
+get "subscriptions", to: "subscriptions#index"
+get "subscriptions/:id", to: "subscriptions#show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
