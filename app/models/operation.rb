59a748b30ef4a63d9f304a6664f79b92e2270f24
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-06-07 11:00:56 +0200

    categorization works - do db:migrate

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,7 +1,7 @@
 class Operation < ApplicationRecord
   belongs_to :user
   belongs_to :subscription, optional: true
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to :account, optional: true
   has_one :operator, through: :subscription
   has_one :bank, through: :account
