d7f586cc8909baaa80de413116a6a7527d7be56d
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-07 19:31:07 +0200

    test, one late night as a team

--- app/services/bridge/users/get_item_refresh_status.rb
+++ app/services/bridge/users/get_item_refresh_status.rb
@@ -7,7 +7,7 @@
 #       client_id: ENV["bridge_client_id"],
 #       client_secret: ENV["bridge_client_secret"]
 #     }
-#      RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"] if Rails.env.production?
+#      #RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"] if Rails.env.production?
 #     response = RestClient.get(url, {
 #       params: params,
 #       "Bankin-Version": "2018-06-15",
