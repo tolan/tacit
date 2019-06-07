5da7145c4ae377fdea2ae4b179f63c65155756b5
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-07 19:50:10 +0200

    test corrections

--- app/services/bridge/items/connect.rb
+++ app/services/bridge/items/connect.rb
@@ -8,7 +8,7 @@ class Bridge::Items::Connect
       client_id: ENV["bridge_client_id"],
       client_secret: ENV["bridge_client_secret"]
     }
-    #RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"] if Rails.env.production?
+    RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"] if Rails.env.production?
     response = RestClient.get(url, {
       params: params,
       "Bankin-Version": "2018-06-15",
