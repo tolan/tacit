a61e61265eb85464b5207557169f8afdccb0552a
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-06-10 16:16:37 +0200

    create a navbar for the show and add condition to display general navbar and footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :demoindex]
+  skip_before_action :authenticate_user!, only: [:home, :demoindex, :demoshow]
 
   def home
   end
