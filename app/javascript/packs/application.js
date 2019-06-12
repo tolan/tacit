45f5db2cf39fdf99ec9e7053ed3c4c7bf28013c3
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-06-12 15:08:51 +0200

    chart ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,19 @@ Chartkick.use(Chart)
 // use built in chartkick method method
 
 // index chart
-const chart_index = document.querySelector('#chart-1');
-const data_index = chart_index.dataset.index;
-const parsed_data_index = JSON.parse(data_index)
-new Chartkick.LineChart("chart-1", parsed_data_index, {colors: ["#FFE8DC", "#004BB4"], xmin: "2018-01-01", xmax: Date.today, points: false, suffix: "€", thousands: " "} )
+if (document.querySelector('#chart-1')){
+  const chart_index = document.querySelector('#chart-1');
+  const data_index = chart_index.dataset.index;
+  const parsed_data_index = JSON.parse(data_index)
+  new Chartkick.LineChart("chart-1", parsed_data_index, {colors: ["#FFE8DC", "#004BB4"], xmin: "2018-01-01", xmax: Date.today, points: false, suffix: "€", thousands: " "} )
+}
 
 
 // // show chart
-const chart_show = document.querySelector('#chart-2');
-const data_show = chart_show.dataset.show;
-const parsed_data_show = JSON.parse(data_show)
-new Chartkick.LineChart("chart-2", parsed_data_show, {colors: ["#FFE8DC", "#004BB4"], xmin: "2018-01-01", xmax: Date.today, points: false, suffix: "€", thousands: " "} )
+if (document.querySelector('#chart-2')){
+  const chart_show = document.querySelector('#chart-2');
+  const data_show = chart_show.dataset.show;
+  const parsed_data_show = JSON.parse(data_show)
+  new Chartkick.AreaChart("chart-2", parsed_data_show, {colors: ["#FFE8DC", "#004BB4"], points: false, suffix: "€", thousands: " "} )
+}
 
