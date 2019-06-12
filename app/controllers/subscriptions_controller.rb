6dd3095c97e5cfcd6f39dba655f5b7eb016d7ad3
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-06-12 09:12:17 +0200

    heroku fix2

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,7 +6,6 @@ class SubscriptionsController < ApplicationController
   end
 
   def show
-    raise
     @user = current_user
     @subscription = Subscription.find(params[:id])
     @last_month_share = @subscription.last_month_spent.to_f / @user.last_month_spent.to_f
