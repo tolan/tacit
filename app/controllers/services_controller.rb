f460a891cff4ca73369636088bee9dd8a148e7b7
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-14 11:33:25 +0200

    added js alert

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,43 +11,23 @@ class ServicesController < ApplicationController
     Converttransaction.call(transactions)
   end
 
-   def google_calendar_item()
-    #check for authentication
-      #no authentication -- poour
-        #missing actions
-
-      #with exisiting authentication, have item that you can then manipulate
-
-      #get form, for variables {opeartion.name, date, start_time, end_time}
+   def google_calendar_item
     service = GoogleCalendar.new.call
-       date = "June 6th, 2020"
-       start_time = "12:00pm"
-       end_time = "1:00pm"
+         date = "June 6th, 2020"
+         start_time = "12:00pm"
+         end_time = "1:00pm"
 
+      # test_variable = 'Rapel: Abonnement Axa Auto'
+      @test_variable = 'This is a another test -- Axa Auto'
 
-    # test_variable = 'Rapel: Abonnement Axa Auto'
-    test_variable = 'This is a another test -- Axa Auto'
+      #reminder 1 - today
+      service.quick_add_event(
+      'primary',
+      "#{@test_variable} on #{Date.today + 1.day} #{start_time}-#{end_time}")
 
-    #reminder 1 - today
-    service.quick_add_event(
-    'primary',
-    "#{test_variable} on #{Date.today + 1.day} #{start_time}-#{end_time}")
-
-
-    service.quick_add_event(
-   'primary',
-   "#{test_variable} on June 6th 2020 #{start_time}-#{end_time}")
-
-  #test mannual
-#
-#  result = service.quick_add_event(
-#  'primary',
-#  'Late Night Test -- on June 14th 9:00am-10:00am')
-#
-#  result2 = service.quick_add_event(
-#    'primary',
-#    'Late Night Test -- on June 6th 2020 9:00am-10:00am')
-#  print result.id
+      service.quick_add_event(
+     'primary',
+     "#{@test_variable} on June 6th 2020 #{start_time}-#{end_time}")
   respond_to do |format|
     format.js
   end
