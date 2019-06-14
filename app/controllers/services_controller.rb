7000e6bb0f516785bb5a69e68a9d79c93b46b90a
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-14 11:01:34 +0200

    added calendar refacto with chrome alert

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -21,23 +21,22 @@ class ServicesController < ApplicationController
       #get form, for variables {opeartion.name, date, start_time, end_time}
     service = GoogleCalendar.new.call
        date = "June 6th, 2020"
-       start_time = "9:00am"
-       end_time = "10:00am"
+       start_time = "12:00pm"
+       end_time = "1:00pm"
 
 
-    test_variable = 'Rapel: Abonnement Axa Auto'
+    # test_variable = 'Rapel: Abonnement Axa Auto'
+    test_variable = 'This is a another test -- Axa Auto'
 
     #reminder 1 - today
-    result = service.quick_add_event(
+    service.quick_add_event(
     'primary',
-    "#{test_variable} on #{Date.today} #{start_time}-#{end_time}")
+    "#{test_variable} on #{Date.today + 1.day} #{start_time}-#{end_time}")
 
 
-
-#reminder 2
-     result = service.quick_add_event(
+    service.quick_add_event(
    'primary',
-   "#{test_variable} on #{date} #{start_time}-#{end_time}")
+   "#{test_variable} on June 6th 2020 #{start_time}-#{end_time}")
 
   #test mannual
 #
@@ -49,5 +48,9 @@ class ServicesController < ApplicationController
 #    'primary',
 #    'Late Night Test -- on June 6th 2020 9:00am-10:00am')
 #  print result.id
+  respond_to do |format|
+    format.js
+  end
+
  end
 end
--- app/services/google_calendar.rb
+++ app/services/google_calendar.rb
@@ -26,7 +26,8 @@ class GoogleCalendar
     client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
     token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
     authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-    user_id = 'default'
+    # user_id = 'default'
+    user_id = 'tolanthornton@gmail.com'
     credentials = authorizer.get_credentials(user_id)
     if credentials.nil?
       url = authorizer.get_authorization_url(base_url: OOB_URI)
