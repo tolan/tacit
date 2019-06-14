6047214e681097fa7c43910176fe79dc73ee057a
Tolan Thornton <taoantaoan@hotmail.com>
2019-06-14 12:00:38 +0200

    another refacto -- chrome alert

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -13,21 +13,22 @@ class ServicesController < ApplicationController
 
    def google_calendar_item
     service = GoogleCalendar.new.call
-         date = "June 6th, 2020"
-         start_time = "12:00pm"
-         end_time = "1:00pm"
+         @renewal_date = " 2020-06-06"
+         @today = Date.today
+         @start_time = "12:00pm"
+         @end_time = "1:00pm"
 
-      # test_variable = 'Rapel: Abonnement Axa Auto'
-      @test_variable = 'This is a another test -- Axa Auto'
+      test_variable = 'Rapel: Abonnement Axa Auto'
+      # @test_variable = 'Tollie test -- Axa Auto'
 
       #reminder 1 - today
       service.quick_add_event(
       'primary',
-      "#{@test_variable} on #{Date.today + 1.day} #{start_time}-#{end_time}")
+      "#{@test_variable} on #{@today} #{@start_time}-#{@end_time}")
 
       service.quick_add_event(
      'primary',
-     "#{@test_variable} on June 6th 2020 #{start_time}-#{end_time}")
+     "#{@test_variable} on #{@renewal_date} #{@start_time}-#{@end_time}")
   respond_to do |format|
     format.js
   end
